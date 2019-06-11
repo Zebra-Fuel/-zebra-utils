5a56907c365dcf0ab2c607d2b94cb4746e5b09f2
Prakash Gurung <prakashgrg18@gmail.com>
2019-06-11 11:42:12 +0100

    ZF-1502 updated mock

--- mockGoogleApi.js
+++ mockGoogleApi.js
@@ -1,16 +1,19 @@
-function mockGoogleApi(formatted_address, lat, lng) {
+function mockGoogleApi(formatted_address, lat, lng, place_id) {
     const location = {
         lat: jest.fn(() => lat),
         lng: jest.fn(() => lng),
     };
     const prediction =
         formatted_address != null
-            ? [[{ structured_formatting: {}, description: formatted_address }]]
+            ? [[{ structured_formatting: {}, description: formatted_address, place_id }]]
             : [[], 'ZERO_RESULTS'];
     const getPlacePredictions = jest.fn((_, autocompleteCallback) =>
         autocompleteCallback(...prediction),
     );
-    const response = lat != null ? [[{ geometry: { location } }]] : [[{ formatted_address }], 'OK'];
+    const response =
+        lat != null
+            ? [[{ geometry: { location }, formatted_address, place_id }]]
+            : [[{ formatted_address, place_id }], 'OK'];
     const geocode = jest.fn((_, callback) => callback(...response));
     return {
         maps: {
