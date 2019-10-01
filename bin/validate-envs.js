5c9f6c45dfdec562184752c9c15a3ab910be10f4
prakash gurung <prakashgrg18@gmail.com>
2019-10-01 16:40:04 +0100

    ZF-1821 wip bin

--- bin/validate-envs.js
+++ bin/validate-envs.js
@@ -3,6 +3,9 @@
 const { REACT_APP_VERSION, SENTRY_ORG, SENTRY_PROJECT } = process.env;
 const envs = { REACT_APP_VERSION, SENTRY_ORG, SENTRY_PROJECT };
 const validateEnv = (key, value) => {
-    if (!value) throw new Error(`${key} not defined`);
+    if (!value) {
+        console.error(`${key} not defined`);
+        throw new Error(`${key} not defined`);
+    }
 };
 Object.keys(envs).map(key => validateEnv(key, envs[key]));
