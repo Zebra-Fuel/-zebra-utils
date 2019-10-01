da0ac93fd83195b6c8755c6193c01a964908e6e7
prakash gurung <prakashgrg18@gmail.com>
2019-10-01 15:37:37 +0100

    ZF-1821 env vars validation

---
+++ validate-envs.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+
+const { REACT_APP_VERSION, SENTRY_ORG, SENTRY_PROJECT } = process.env;
+const envs = { REACT_APP_VERSION, SENTRY_ORG, SENTRY_PROJECT };
+const validateEnv = (key, value) => {
+    if (!value) throw new Error(`${key} not defined`);
+};
+Object.keys(envs).map(key => validateEnv(key, envs[key]));
